d8d8c78374918024f1604f220606a686bd7f82b8
Abdallah Sow <abdallahsow700@gmail.com>
2022-10-06 15:58:27 +0300

    added a title to the posts' page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Link from "next/link";
 import Tool from "/components/Tool/Tool";
 import { toolsData } from "/utils/data/toolsData";
@@ -5,6 +6,10 @@ import { toolsData } from "/utils/data/toolsData";
 const FirstPost = () => {
   return (
     <>
+      <Head>
+        <title>Front-End</title>
+      </Head>
+
       <h1>Popular Front-End Development Tools</h1>
       <br />
       {toolsData.map((tool, index) => (
